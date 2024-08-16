d891669ed79b179cff2623f27f2c59b4135b555a
IshworKhatiwada <IshworKhatiwada@DOGMA-F0X04F3>
2024-08-16 14:14:29 +0545

    Add project files.

---
+++ Practice_web_api/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice_web_api.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<Product> _product = new();
+        static ProductController()
+        {
+            _product.Add(new Product { Id = 1, Name = "Electronics", Price = 2000 });
+
+        }
+
+        [HttpPost]
+        public IActionResult postdata([FromBody] Product product)
+        {
+            _product.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+
+        }
+
+
+
+
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _product;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetbyID(int id)
+        {
+            var product = _product.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put (int id, [FromBody] Product product)
+        {
+            var existingproduct = _product.FirstOrDefault(y => y.Id == id);
+            if (existingproduct == null)
+            {
+                return NotFound();
+            }
+            existingproduct.Name = product.Name;
+            existingproduct.Price = product.Price;
+            return Ok(existingproduct);
+        }
+    }
+}
---
+++ Practice_web_api/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice_web_api
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
